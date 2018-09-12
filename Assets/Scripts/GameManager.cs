9b8dbc586856e4314fa5929aba87bccc9cb2e5f4
Yannick Robert <yrobert@apption.com>
2018-09-11 20:10:02 -0400

    wip on network movement

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class GameManager : NetworkBehaviour {
+
+    [SyncVar] public float TimeOfDay = 1200;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!isServer)
+        {
+            return;
+        }
+
+        TimeOfDay += Time.deltaTime;
+        if(TimeOfDay >= 2400)
+        {
+            TimeOfDay = 0;
+        }
+
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerController : NetworkBehaviour {
+
+    float moveSpeed = 2.5f;
+
+
+    [SyncVar] Vector3 serverPosition;
+    Vector3 serverPositionSmoothVelocity;
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (isServer)
+        {
+            // server-specific checking like taking ongoing damage
+        }
+
+        if (hasAuthority == true)
+        {
+            AuthorityUpdate();
+        }
+
+        // Do generic updates for ALL clients / server -- like animating movements and such
+
+        // Are we in the correct position?
+        if (hasAuthority == false)
+        {
+            // Lerp towards the "correct" server position
+            transform.position = Vector3.SmoothDamp(transform.position, serverPosition, ref serverPositionSmoothVelocity, 0.25f);
+        }
+
+
+	}
+
+    void AuthorityUpdate()
+    {
+        Vector3 movement = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"), 0f).normalized;
+        transform.Translate(movement * Time.deltaTime * moveSpeed);
+
+        CmdUpdatePosition(transform.position);
+    }
+
+    [Command]
+    void CmdUpdatePosition(Vector3 newPosition)
+    {
+        serverPosition = newPosition;
+    }
+}
